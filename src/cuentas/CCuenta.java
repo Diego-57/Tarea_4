c3f750e4f1c550a5f804109ad1114da1390612c5
Diego <Diego@192.168.56.1>
2025-03-03 22:24:26 +0100

    Comentarios javadoc

--- src/cuentas/CCuenta.java
+++ src/cuentas/CCuenta.java
@@ -9,26 +9,26 @@ public class CCuenta {
     public CCuenta()
     {
     }
-
+/**Metodo constructor*/
     public CCuenta(String nom, String cue, double sal, double tipo)
     {
         nombre =nom;
         cuenta=cue;
         saldo=sal;
     }
-
+/**Metodo para devolver el estado/saldo de la cuenta*/
     public double estado()
     {
         return saldo;
     }
-
+/**Metodo para ingresar en la cuenta*/
     public void ingresar(double cantidad) throws Exception
     {
         if (cantidad<0)
             throw new Exception("No se puede ingresar una cantidad negativa");
         saldo = saldo + cantidad;
     }
-
+/**Metodo para retirar de la cuenta*/
     public void retirar(double cantidad) throws Exception
     {
         if (cantidad <= 0)
@@ -37,35 +37,35 @@ public class CCuenta {
             throw new Exception ("No se hay suficiente saldo");
         saldo = saldo - cantidad;
     }
-
+/**Getter del nombre*/
     public String getNombre() {
         return nombre;
     }
-
+/**Getter de la cuenta*/
     public String getCuenta() {
         return cuenta;
     }
-
+/**Getter del saldo*/
     public double getSaldo() {
         return saldo;
     }
-
+/**Getter del tipo de interes*/
     public double getTipoInterés() {
         return tipoInterés;
     }
-
+/**Setter del nombre*/
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
-
+/**Setter de la cuenta*/
     public void setCuenta(String cuenta) {
         this.cuenta = cuenta;
     }
-
+/**Setter del saldo*/
     public void setSaldo(double saldo) {
         this.saldo = saldo;
     }
-
+/**Setter del tipo de interes*/
     public void setTipoInterés(double tipoInterés) {
         this.tipoInterés = tipoInterés;
     }
